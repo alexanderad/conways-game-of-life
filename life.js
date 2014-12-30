195ef1864ddbeb5c098572d181aef24ecc8e91d6
Alexander Shchapov <alexanderad@gmail.com>
2014-12-30 10:25:01 +0700

    add package.json, add TorusArray tests

--- life.js
+++ life.js
@@ -1,1 +1,9 @@
+var http = require('http');
 var TorusArray = require('./torus-array.js');
+
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("hello");
+    response.end();
+}).listen(2100);
---
+++ test/torus-array.js
@@ -0,0 +1,45 @@
+var assert = require('chai').assert,
+    TorusArray = require('../life/torus-array.js');
+
+describe('TorusArray', function () {
+    describe('public and private properties', function () {
+        it('width and height are public', function () {
+            var ta = new TorusArray(5, 10);
+            assert.equal(ta.width, 5);
+            assert.equal(ta.height, 10);
+        });
+        it('grid is private', function () {
+            var ta = new TorusArray(5, 10);
+            assert.isUndefined(ta.grid);
+        });
+    });
+    describe('public methods', function () {
+        it('simple set()', function() {
+            var ta = new TorusArray(2, 2);
+            ta.set(0, 0, 5);
+            assert.equal(ta.get(0, 0), 5);
+        });
+        it('negative index set() + get()', function() {
+            var ta = new TorusArray(3, 3);
+            ta.set(-1, 0, 5);
+            ta.set(0, -1, 7);
+            
+            assert.equal(ta.get(-1, 0), 5);
+            assert.equal(ta.get(2, 0), 5);
+
+            assert.equal(ta.get(0, -1), 7);
+            assert.equal(ta.get(0, 2), 7);
+        });
+        it('positive out of bounds set() + get()', function() {
+            var ta = new TorusArray(3, 3);
+            ta.set(5, 0, 10);
+            ta.set(0, 5, 7);
+            
+            assert.equal(ta.get(5, 0), 10);
+            assert.equal(ta.get(2, 0), 10);    
+
+            assert.equal(ta.get(0, 5), 7);
+            assert.equal(ta.get(0, 2), 7);  
+        });
+    });
+});
